e48a5daba07e9129d68a4b3b7f0f9c508448d79a
Patrick Columbro <pcolumbro@users.noreply.github.com>
2023-10-06 17:57:36 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container>
+        <Typography variant="h1" component="div" gutterBottom>
+          h1. Heading
+        </Typography>
+    </Container>
+  );
+}
+export default App
