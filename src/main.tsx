f7d2d3442b90516500911096cd322433c490aa4b
Patrick Columbro <pcolumbro@users.noreply.github.com>
2023-10-07 21:37:03 -0700

    fix import

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import App from './App';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline, createTheme } from '@mui/material';
+import App from './app';
 
 const theme = createTheme({
   palette: {
