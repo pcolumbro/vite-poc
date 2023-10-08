b65c5eb838d4bfe58767b98a71ca6cac27aef6e8
Patrick Columbro <pcolumbro@users.noreply.github.com>
2023-10-08 16:39:54 -0700

    links

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # React + TypeScript + Vite + Redux Toolkit
 
-hosted at https://vite-poc.pages.dev/
+hosted on [Cloudflare Pages](https://vite-poc.pages.dev/)
+
+
+1. [Redux Essentials](https://redux.js.org/tutorials/essentials/part-1-overview-concepts)
+2. [When to use Redux blogpost](https://changelog.com/posts/when-and-when-not-to-reach-for-redux)
+3. [Reddit post from Redux Maintainer](https://www.reddit.com/r/reactjs/comments/z4y7sj/comment/ixtfehr/?utm_source=share&utm_medium=web2x&context=3)
--- src/features/user-demo.tsx
+++ src/features/user-demo.tsx
@@ -34,20 +34,10 @@ const UserDemo = () => {
                 <Grid item xs={12} md={3}>
                     <Card>
                         <CardContent>
-                            <Stack sx={{display: 'flex', justifyContent: 'center'}}>
+                            <Stack spacing={1} sx={{display: 'flex', justifyContent: 'center'}}>
                                 <Typography variant="h5">Actions</Typography>
                                 <Button onClick={() => dispatch({ type: 'user/populate' })}>Populate User</Button>
                                 <Button onClick={() => dispatch({ type: 'user/clear' })}>Clear User</Button>
-                                {/* <Typography variant="h5" sx={{paddingTop: 5}}>Links</Typography>
-                                <Box sx={{display: 'flex', justifyContent: 'center'}}>
-                                    <Stack spacing={2}>
-                                        <a style={linkStyle} href="https://github.com/pcolumbro/vite-poc" target="_blank">View Source</a>
-                                        <a style={linkStyle} href="https://redux.js.org/tutorials/essentials/part-1-overview-concepts" target="_blank">Redux Essential Reading</a>
-                                        <a style={linkStyle} href="https://changelog.com/posts/when-and-when-not-to-reach-for-redux" target="_blank">When to use Redux</a>
-                                        <a style={linkStyle} href="https://www.reddit.com/r/reactjs/comments/ql3br1/comment/hj0ic6z/?utm_source=share&utm_medium=web2x&context=3" target="_blank">Reddit post 1 from Redux Maintainer</a>
-                                        <a style={linkStyle} href="https://www.reddit.com/r/reactjs/comments/z4y7sj/comment/ixtfehr/?utm_source=share&utm_medium=web2x&context=3" target="_blank">Reddit post 2 from Redux Maintainer</a>
-                                    </Stack>
-                                </Box> */}
                             </Stack>
                         </CardContent>
                     </Card>
