22fa905955e0f8589348dfeded9f42459ec29154
Patrick Columbro <pcolumbro@users.noreply.github.com>
2023-10-08 16:27:36 -0700

    remove links

--- src/features/user-demo.tsx
+++ src/features/user-demo.tsx
@@ -38,7 +38,7 @@ const UserDemo = () => {
                                 <Typography variant="h5">Actions</Typography>
                                 <Button onClick={() => dispatch({ type: 'user/populate' })}>Populate User</Button>
                                 <Button onClick={() => dispatch({ type: 'user/clear' })}>Clear User</Button>
-                                <Typography variant="h5" sx={{paddingTop: 5}}>Links</Typography>
+                                {/* <Typography variant="h5" sx={{paddingTop: 5}}>Links</Typography>
                                 <Box sx={{display: 'flex', justifyContent: 'center'}}>
                                     <Stack spacing={2}>
                                         <a style={linkStyle} href="https://github.com/pcolumbro/vite-poc" target="_blank">View Source</a>
@@ -47,7 +47,7 @@ const UserDemo = () => {
                                         <a style={linkStyle} href="https://www.reddit.com/r/reactjs/comments/ql3br1/comment/hj0ic6z/?utm_source=share&utm_medium=web2x&context=3" target="_blank">Reddit post 1 from Redux Maintainer</a>
                                         <a style={linkStyle} href="https://www.reddit.com/r/reactjs/comments/z4y7sj/comment/ixtfehr/?utm_source=share&utm_medium=web2x&context=3" target="_blank">Reddit post 2 from Redux Maintainer</a>
                                     </Stack>
-                                </Box>
+                                </Box> */}
                             </Stack>
                         </CardContent>
                     </Card>
